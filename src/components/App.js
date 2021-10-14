d495887b3c2d216d9239c25e792e142467b7d6d7
jinish08 <jinishshah08@gmailcom>
2021-10-14 18:30:22 +0530

    installed and added bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,25 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Home, ProductDetails } from '../pages';
-import { Navbar } from './';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import { Home, ProductDetails } from "../pages";
+import { Navbar } from "./";
 
 function App() {
-	return (
-		<div className="App">
-			<Router>
-				<Navbar />
-				<Switch>
-					<Route exact path="/">
-						<Home />
-					</Route>
-					<Route exact path="/product/:productId">
-						<ProductDetails />
-					</Route>
-				</Switch>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/product/:productId">
+            <ProductDetails />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
