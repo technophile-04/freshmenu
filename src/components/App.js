a3a22e774f8bd59f4850e6496b75798380a1e0a3
Shiv Bhonde <shivbhonde04@gmail.com>
2021-10-14 10:54:46 +0530

    add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home, ProductDetails } from '../pages';
+import { Navbar } from './';
+
 function App() {
-	return <div className="App">Food menu</div>;
+	return (
+		<div className="App">
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route exact path="/product/:productId">
+						<ProductDetails />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
