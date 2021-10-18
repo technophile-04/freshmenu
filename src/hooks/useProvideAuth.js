36597f68f41426b78915a7e04702dde389f3ab3c
Shiv Bhonde <shivbhonde04@gmail.com>
2021-10-18 14:45:27 +0530

    persist user

--- src/hooks/useProvideAuth.js
+++ src/hooks/useProvideAuth.js
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
 	signInWithEmailAndPassword,
 	createUserWithEmailAndPassword,
+	onAuthStateChanged,
 } from 'firebase/auth';
 import { auth } from '../utils/init-firebase';
 
 const useProvideAuth = () => {
 	const [currentUser, setCurrentUser] = useState(null);
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (user) => {
+			setCurrentUser(user);
+		});
+		return () => {
+			unsubscribe();
+		};
+	}, []);
 
 	const register = (email, password) => {
 		return createUserWithEmailAndPassword(auth, email, password);
